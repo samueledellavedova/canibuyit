cb5e5dbbf6a45878042767f7462df6da3df9302d
samueledellavedova <46026355+samueledellavedova@users.noreply.github.com>
2021-12-15 23:27:38 +0100

    chore: initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+  double bal = 0;
+
+  printf("Sir, what's your balance? ");
+  scanf("%lf", &bal);
+
+  if (bal <= 0) {
+    printf("The only thing you can afford is this dumb program.\n");
+    return 0;
+  }
+
+  double price = 0;
+
+  printf("Now tell me, how much does the thing you want to buy cost? ");
+  scanf("%lf", &price);
+
+  if (price < 0) {
+    printf("I don't think that this is a valid price.\n");
+    return 0;
+  }
+
+  if (price <= bal / 10) {
+    printf("Yeah, i think you can afford it.\n");
+  } else {
+    printf("Nope, you can't afford it.\n");
+  }
+
+  return 0;
+}
